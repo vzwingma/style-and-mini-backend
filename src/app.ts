dd5f483fe66d20ecbd604558bb74b29180214441
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2025-03-10 16:39:16 +0100

    retour de app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,33 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
+import * as middlewares from './api/interfaces/middlewares';
+import api from './api';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 dotenv.config();
+
+
+export const app = express();
+
+const startApp = () => {
+  app.use(morgan('dev'));
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.json());
+
+
+  app.use('/api/v1', api);
+
+  app.use(middlewares.notFound);
+  app.use(middlewares.errorHandler);
+
+};
+startApp();
+
 
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
