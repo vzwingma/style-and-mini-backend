b8ab318395a3bec829706eaedbf8a4687b4aecdc
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2025-03-10 16:07:38 +0100

    test deploy nodejs

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import dotenv from 'dotenv';
 
 import * as middlewares from './api/interfaces/middlewares';
 import api from './api';
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 dotenv.config();
 
@@ -27,23 +26,3 @@ const startApp = () => {
 
 };
 startApp();
-
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('AWS Lambda event', event);
-  try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello Style And Mini',
-      }),
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'some error happened',
-      }),
-    };
-  }
-};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { app } from './app';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+// import { app } from './app';
 import { BACKEND_PORT } from './constants/AppConstants';
-
+/*
 const port = BACKEND_PORT;
 app.listen(port, () => {
-  /* eslint-disable no-console */
+
   console.log(`Listening: http://0.0.0.0:${port}`);
-  /* eslint-enable no-console */
+
 });
+*/
+
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('AWS Lambda event', event);
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello Style And Mini',
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'some error happened',
+      }),
+    };
+  }
+};
