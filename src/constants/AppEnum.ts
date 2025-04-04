513dba8e204d1bc0e827162fea3d9a9d452218b9
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2025-04-05 00:04:08 +0200

    v0.2.1

--- src/constants/AppEnum.ts
+++ src/constants/AppEnum.ts
@@ -1,5 +1,5 @@
-
-export const APP_MOBILE_VERSION = '0.2.0';
+const packageJSON = require("./../../package.json");
+export const APP_MOBILE_VERSION = packageJSON.version;
 export const APP_MOBILE_NAME = 'Style et Mini';
 // Enumération des statuts de l'application
 
