43be2ba14b55602a038be2936503f18b7f22d54b
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2026-06-01 11:36:19 +0200

    fix: expose error stack in error handler for better debugging

--- src/api/interfaces/middlewares.ts
+++ src/api/interfaces/middlewares.ts
@@ -14,6 +14,6 @@ export function errorHandler(err: Error, req: Request, res: Response<ErrorRespon
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    stack: err.stack,
   });
 }
